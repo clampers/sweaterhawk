3cd5a9df80690275345c68aa3ec31914b39ad246
Andrew Barrett <brazenbarrett@gmail.com>
2018-01-30 22:47:37 -0600

    set up controllers and templates

---
+++ src/main/java/com/sweaterhawk/sweaterhawk/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.sweaterhawk.sweaterhawk.controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+@RequestMapping(value = "")
+public class HomeController {
+
+    @RequestMapping(value="")
+    public String index(Model model){
+        model.addAttribute("title", "pooooops");
+        return "test";
+    }
+
+}
